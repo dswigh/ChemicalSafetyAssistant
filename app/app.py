58b3d938751fee45aefcd2756875aba6b4c56401
gelzinyte <elena.gelzinyte@gmail.com>
2022-09-22 16:00:55 +0100

    data in pandas

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from backend import get_fake_data, get_data
+from backend import get_data
 from utilities import query_id_types
 
 query = st.text_input("query", value="")
@@ -8,25 +8,20 @@ query_id_type = st.selectbox("query type", query_id_types)
 if len(query) > 0 :
 
     df, hcode_descriptions, hcode_pics = get_data(query, query_id_type)
-    # d = get_fake_data(query)
 
     st.header(f'Information on {query}:')
 
-    st.write(f'{query_id_type}: {d.name}')
-    st.write(f'CAS number: {d.cas_number}')
-    st.write(f'molecular weight: {d.mol_wt} g/mol')
-    st.write(f'density: {d.density} g/ml')
-    st.write(f'hazard codes: {d.hazard_codes}')
+    st.write(df)
 
-    if len(d.hazard_code_descriptions) > 0:
+    if len(hcode_descriptions) > 0:
         with st.expander(f'Hazard code explanations'):
         
-            cols = st.columns(len(d.hazard_image_urls))
-            for img, col in zip(d.hazard_image_urls, cols):
+            cols = st.columns(len(hcode_pics))
+            for img, col in zip(hcode_pics, cols):
                 with col:
                     st.image(img)
 
-            for expl in d.hazard_code_descriptions:
+            for expl in hcode_descriptions:
                 st.write(expl)
 
 else:
--- app/backend.py
+++ app/backend.py
@@ -4,40 +4,26 @@ import utilities
 
 col_names = ["name", "mol wt", "CAS", "density", "hazard codes"]
 
-def get_fake_data(query_name):
-    """This should be replaced by PubChem query"""
-
-    data = CompoundInfo(
-        name = "water",
-        mol_wt = 18.0,
-        cas_number = 1234,
-        density = 1.0, 
-        hazard_codes = ['H123', 'H455']
-    )
-
-    return data
 
 def get_data(query, query_id_type):
     url = utilities.get_url(utilities._get_cid(query, query_id_type))
     json = utilities.get_json(url)
     hcodes, hdescriptions, pictures = utilities.get_hazard_info(json)
+
+    if hcodes is None:
+        hcodes = ["None found"]
+        hdescriptions = []
+        pictures = []
     
     df = pd.DataFrame(columns=col_names) 
-    df["name"] = f"{query} ({query_id_type})"
-    df["mol wt"] = 18.0
-    df["CAS"] = "12-45-643"
-    df["density"] = 1.0
-    df["hazard codes"] = ', '.join(hcodes)
+    row = pd.Series(
+        {   query_id_type : f"{query}",
+            "mol wt" : 18.0,
+            "CAS" : "12-45-643",
+            "density" : 1.0,
+            "hazard codes" : ', '.join(hcodes)})
+    df = df.append(row, ignore_index=True)
 
-    data = CompoundInfo(
-        name = 'query',
-        mol_wt = 18.0,
-        cas_number = 1234,
-        density = 1.0, 
-        hazard_codes = hcodes, 
-        hazard_code_descriptions = hdescriptions,
-        hazard_image_urls = pictures 
-    )    
-    return data, hdescriptions, pictures
+    return df, hdescriptions, pictures
 
 
