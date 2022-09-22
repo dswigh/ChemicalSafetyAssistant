b89ac1981f0860306eca94237cd18c5d9f2c002a
gelzinyte <elena.gelzinyte@gmail.com>
2022-09-22 15:31:37 +0100

    aligning pictures and formating

--- app/app.py
+++ app/app.py
@@ -12,21 +12,30 @@ if len(query) > 0 :
 
     st.header(f'Information on {query}:')
 
-    st.text(f'name: {d.name}')
-    st.text(f'CAS number: {d.cas_number}')
-    st.text(f'molecular weight: {d.mol_wt} g/mol')
-    st.text(f'density: {d.density} g/ml')
-    st.text(f'hazard codes: {d.hazard_codes}')
+    st.write(f'{query_id_type}: {d.name}')
+    st.write(f'CAS number: {d.cas_number}')
+    st.write(f'molecular weight: {d.mol_wt} g/mol')
+    st.write(f'density: {d.density} g/ml')
+    st.write(f'hazard codes: {d.hazard_codes}')
 
     st.subheader(f'Hazard code explanations')
     
-    for pic in d.hazard_image_urls:
-        st.image(pic)
-
-    st.text(d.hazard_code_descriptions)
-    # for expl in d.hazard_code_descriptions:
+    # for pic in d.hazard_image_urls:
     #     st.image(pic)
-    #     st.text(expl)
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image(d.hazard_image_urls[0])
+    with col2:
+        st.image(d.hazard_image_urls[1])
+
+
+
+
+
+    for expl in d.hazard_code_descriptions:
+        st.write(expl)
 
 else:
     print('Enter chemical name to find information')
